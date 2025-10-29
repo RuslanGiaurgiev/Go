94a663c9664d6e1489c26ce69f2c8288213486df
synt3x <gaurgievruslan89@gmail.com>
2025-10-29 15:01:56 +0300

    update

--- les11/test/main.go
+++ les11/test/main.go
@@ -9,7 +9,12 @@ import (
 )
 
 var number1 int = 0
+
 var number2 atomic.Int64
+
+var slice []int
+
+var mtx sync.Mutex
 
 func increase1(wg1 *sync.WaitGroup) {
 	defer wg1.Done()
@@ -27,9 +32,20 @@ func increase2(wg2 *sync.WaitGroup) {
 	}
 }
 
+func increase3(wg3 *sync.WaitGroup) {
+	defer wg3.Done()
+
+	for i := 0; i < 2000; i++ {
+		mtx.Lock()
+		slice = append(slice, i)
+		mtx.Unlock()
+	}
+}
+
 func main() {
 	wg1 := &sync.WaitGroup{}
 	wg2 := &sync.WaitGroup{}
+	wg3 := &sync.WaitGroup{}
 
 	wg1.Add(5)
 	go increase1(wg1)
@@ -50,5 +66,14 @@ func main() {
 
 	wg2.Wait()
 	fmt.Println("Число на исходе", number2.Load())
+
+	wg3.Add(5)
+	go increase3(wg3)
+	go increase3(wg3)
+	go increase3(wg3)
+	go increase3(wg3)
+	go increase3(wg3)
+	wg3.Wait()
+	fmt.Println("len slice:", len(slice))
 
 }
